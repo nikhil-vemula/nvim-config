dce3ee81e5bcac330a2f50df1dc853b9e1b49309
Nikhil Vemula <vsrinivasanikhil@gmail.com>
2025-03-01 13:31:17 -0800

    Close neotree on file open

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,7 +8,17 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+      require("neo-tree").setup({
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+        },
+      })
+      vim.keymap.set('n', '//', ':Neotree filesystem reveal left<CR>')
     end
   }
 }
